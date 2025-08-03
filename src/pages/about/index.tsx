8d5c54e8cc707fa0b4556e04e10e679f787e9b98
suke <oak1990@163.com>
2025-08-04 02:49:45 +0800

    docs: 添加关于页面
    - 新增 About 组件以介绍应用的功能和使用方法
    - 页面内容包括应用概述、功能说明和感谢用户使用的信息

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,11 @@
+"use client"
+export default function About() {
+  return (
+    <div>
+      <h1>关于我们</h1>
+      <p>这是一个简单的备忘录和待办事项应用。</p>
+      <p>您可以在这里添加、编辑和删除您的备忘录和待办事项。</p>
+      <p>感谢您的使用！</p>
+    </div>
+  )
+}
